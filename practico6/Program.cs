44c7f3215c801e4a1754d9904f8473447bd5b5f4
Mauricio <maurijo.suarez@gmail.com>
2023-05-18 19:21:01 -0300

    ejercicio 2

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -10,10 +10,10 @@ Console.WriteLine("valor de b:"+b);
 /*Ejercicio 1. Construir un programa que permita invertir un número. Verifique que el
 texto ingresado es de hecho un número y, en caso afirmativo, realice la inversión del
 número sólo si éste es mayor a 0.*/
-string? s;
+
+/*string? s;
 int n, resto;
 bool resultado;
-
 Console.WriteLine("Ingrese un numero: ");
 s = Console.ReadLine();
 
@@ -30,4 +30,59 @@ if (resultado)
         }
 
     }
+}  */
+
+Console.WriteLine("Operacion: \n1-Sumar\n2-Restar\n3-Multiplicar\n4-Dividir");
+string? opcion = Console.ReadLine();
+Console.WriteLine("Ingrese los dos numeros a operar");
+string? operando1 = Console.ReadLine();
+string? operando2 = Console.ReadLine();
+int op, n1, n2;
+
+float result;
+if(int.TryParse(operando1, out n1) && int.TryParse(operando2, out n2)  && int.TryParse(opcion, out op) && op<4 && op >=0)
+{
+    Console.WriteLine("El resultado es: ");
+    switch (op)
+    {
+        case 1:
+            result = sumar(n1, n2);
+            Console.Write(result);
+            break;
+        case 2:
+            result = restar(n1, n2);
+            Console.Write(result);
+            break;
+        case 3:
+            result = multiplicar(n1, n2);
+            Console.Write(result);
+            break;
+        case 4:
+            result = dividir(n1, n2);
+            Console.Write(result);
+            break;
+    }
+
+}
+//declaracion de funciones
+float dividir(int a, int b)
+{
+    float resultado = (float)a / b;
+    return resultado;
+
+}
+int sumar(int a, int b)
+{
+    return a + b;
+
+}
+float restar(int a, int b)
+{
+    return a - b;
+
+}
+float multiplicar(int a, int b)
+{
+    return a * b;
+
 }
